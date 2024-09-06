5d3a180b7cdde55033196c58c60985b435700705
Unchain-X <unchainx.aws@gmail.com>
2024-09-06 11:25:38 +0900

    update specific imports

--- contracts/RelayERC20.sol
+++ contracts/RelayERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {IERC20Mintable} from './interface/IERC20Mintable.sol';
 import {ICommonError} from './common/ICommonError.sol';
 
